2f6f4a7341d3058a455e2a275e977871b1f936b2
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2024-12-16 21:36:51 +0000

    temp st

--- src/server/commands/tcp/showtrials.cpp
+++ src/server/commands/tcp/showtrials.cpp
@@ -1,31 +1,4 @@
 #include "showtrials.hpp"
-int FindLastGame(const std::string& PLID, std::string& fname) {
-    namespace fs = std::filesystem;
-
-    std::string dirname = "GAMES/" + PLID + "/";
-    fs::path dirPath(dirname);
-
-    if (!fs::exists(dirPath) || !fs::is_directory(dirPath)) {
-        return 0;
-    }
-
-    fs::path lastFile;
-    bool found = false;
-
-    for (const auto& entry : fs::directory_iterator(dirPath)) {
-        if (entry.is_regular_file() && entry.path().filename().string()[0] != '.') {
-            lastFile = entry.path();
-            found = true;
-        }
-    }
-
-    if (found) {
-        fname = lastFile.string();
-    }
-
-    return found ? 1 : 0;
-}
-
 
 bool ShowTrialsCommand::check() {
     if (this->plid.length() != 6 || !std::all_of(this->plid.begin(), this->plid.end(), ::isdigit)) {
@@ -35,53 +8,129 @@ bool ShowTrialsCommand::check() {
 }
 
 std::string ShowTrialsCommand::exec() {
-    std::string playerInfo = this->findPlayerInfo(this->plid);
-    std::string response;
 
-    namespace fs = std::filesystem;
+    bool currentGame = false;
+
+    std::string result;
     std::string fileName;
-    fs::path filePath;
-    std::string status;
 
-    if (playerInfo != ""){
-        fileName = "GAME_" + this->plid + ".txt";   
-        filePath = (fs::path)(DB_GAMES_PATH + '/' + fileName);
-        status = ACT;
+    std::string playerInfo = this->findPlayerInfo(this->plid);
+    if (playerInfo != "") {
+        fileName = (std::string)DB_GAMES_PATH + "/GAME_" + this->plid + ".txt";
+        result = "RST ACT ";
+        currentGame = true;
     }
     else {
-        if(FindLastGame(this->plid, fileName) == 0)
-            return SHOW_TRIALS_RESP + ' ' + (std::string)NOK;
-
-        filePath = (fs::path)(DB_GAMES_PATH + '/' + this->plid + '/' + fileName);
-        status = FIN;
+        std::string dirPath = (std::string)DB_GAMES_PATH + "/" + this->plid;
+        if (std::filesystem::exists(dirPath) && std::filesystem::is_directory(dirPath)) {
+            
+            std::vector<std::filesystem::directory_entry> files;
+            for (const auto& entry : std::filesystem::directory_iterator(dirPath)) {
+                if (entry.is_regular_file()) {
+                    files.push_back(entry);
+                }
+            }
+            std::sort(files.begin(), files.end(), [](const std::filesystem::directory_entry& a, const std::filesystem::directory_entry& b) {
+                return a.path().filename() > b.path().filename();
+            });
+            fileName = files[0].path();
+            result = "RST FIN ";
+        }
+        else 
+            return "RST NOK\n";
     }
 
+    std::string trialsFileName = "trials_" + this->plid + ".txt";
+    std::ofstream trials(trialsFileName);
+    if (!trials.is_open()) {
+        perror("Unable to open file");
+        exit(1);
+    }
+
+    std::string to_write = currentGame ? "Active game found for player " : "Last finalized game for player ";
+
+    printf("File name: %s\n", fileName.c_str());
     std::ifstream file(fileName);
     if (!file.is_open()) {
         perror("Unable to open file");
         exit(1);
     }
-
-    auto size = std::filesystem::file_size(filePath);
-    response = SHOW_TRIALS_RESP + ' ' + status + ' ' + fileName + ' ' + std::to_string(size) + ' ';
+    std::getline(file, playerInfo);
     
-    std::string line;
-    std::getline(file, line);
-    while(std::getline(file, line)){
-        response += line;
-    }
-    file.close();
-
-    //na ultima linha aparece o tempo que ainda têm para jogar
+    printf("Player info: %s\n", playerInfo.c_str());
+    fflush(stdout);
     std::istringstream iss(playerInfo);
-    std::vector<std::string> args;
     std::string arg;
-    while (iss >> arg) {
-        args.push_back(arg);
+    std::vector<std::string> args;
+    while (iss >> arg) { args.push_back(arg); }
+
+    trials << std::endl << to_write + this->plid << std::endl;
+    trials << "Game initiated: " + args[4] + " " + args[5] + " with " + args[3] + " seconds to be completed" << std::endl;
+    
+    if (!currentGame) {
+        trials << "Mode : " << ((args[1] == "P") ? "PLAY" : "DEBUG");
+        trials << "  Secret code: " + args[2] << std::endl;
+        std::string secretKey = args[1] + " " + args[2] + " " + args[3] + " " + args[6];
     }
 
-    std::time_t sec = std::time(nullptr);
-    response += (std::to_string)(std::stoi(args[3]) - (std::stoi(args[6]) - sec));
-    return response + "\n";
-}
+    std::vector<std::string> tries;
+    while (std::getline(file, playerInfo)) {
+        if (playerInfo[0] == 'T') {
+            tries.push_back(playerInfo);
+        }
+    }
+    
+    trials << std::endl << "     --- Transactions found: " + std::to_string(tries.size()) + " ---" << std::endl << std::endl;
 
+    for (std::string t : tries) {
+
+        std::istringstream iss(t);
+        std::string arg_t;
+        std::vector<std::string> args_t;
+        while (iss >> arg_t) { args_t.push_back(arg_t); }
+        trials << "Trial: " << args_t[1] + ", nB: " + args_t[2] + ", nW: " + args_t[3] + " at " + args_t[4] + "s" << std::endl;
+    }
+
+    if (currentGame) {
+        time_t now = std::time(nullptr);
+        time_t time_remaining = std::stoi(args[3]) - now + std::stoi(args[6]);
+        trials << std::endl << "  -- " + std::to_string(time_remaining) + " seconds remaining to be completed --" << std::endl; 
+    }
+    else {
+        std::ifstream file(fileName);
+        if (!file.is_open()) {
+            perror("Unable to open file");
+            exit(1);
+        }
+        std::string line;
+        std::string lastLine;
+        while (std::getline(file, line)) {
+            lastLine = line;
+        }
+        file.close();
+        std::istringstream iss(lastLine);
+        std::vector<std::string> args;
+        std::string arg;
+        while (iss >> arg) { args.push_back(arg); }
+        trials << std::endl << "  -- Game completed at " + args[0] + " " + args[1] + ", Duration: " + args[2] + "s" << std::endl;
+    }
+
+    trials.close();
+
+    std::ifstream r_trials(trialsFileName.c_str());
+    if (!r_trials.is_open()) {
+        perror("Unable to open file");
+        exit(1);
+    }
+
+    std::stringstream content;
+    content << r_trials.rdbuf();
+
+    printf("Content: %s\n", content.str().c_str());
+
+    r_trials.close();
+
+    auto size = std::filesystem::file_size(trialsFileName);
+
+    return result + trialsFileName + " " + std::to_string(size) + " " + content.str();
+}
