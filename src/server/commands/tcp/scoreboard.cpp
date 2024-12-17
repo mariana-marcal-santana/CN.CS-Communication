cb83db002d0ddb1c207009544fb141a4673e97ba
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2024-12-17 18:29:24 +0000

    test sb

--- src/server/commands/tcp/scoreboard.cpp
+++ src/server/commands/tcp/scoreboard.cpp
@@ -77,5 +77,5 @@ std::string ScoreboardCommand::exec() {
         std::perror("Error deleting file");
     }
 
-    return "RSS OK " + scoreboardFileName + " " + std::to_string(size) + " " + content.str() + "\n";
+    return "RSS OK " + scoreboardFileName + " " + std::to_string(size) + " " + content.str();
 }
